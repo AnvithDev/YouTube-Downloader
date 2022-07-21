1ddf1154f6b6335675af4ad2a330dd0dbcc16b5e
AnvithDev <anvith1919@gmail.com>
2022-07-21 16:02:30 +0530

    add base route

--- website/views.py
+++ website/views.py
@@ -1,13 +1,21 @@
-from flask import Blueprint, render_template, request
-from yt_download import yt_download
+from flask import Blueprint, render_template, request, session, url_for,redirect
+from textwrap import indent
+from pytube import YouTube
+from sys import argv
+import pprint
 
 views = Blueprint('views', __name__)
 
 @views.route('/', methods=['GET','POST'])
 def home():
     if(request.method == 'POST'):
-        yt_url = request.form.get('yt_url')
+        session['link'] = request.form.get('yt_url')
+        try:
+            yt_url=YouTube(session['link'] )
+            yt_url.check_availability()
+        except:
+            return render_template("error.html")
+        return render_template("download.html", yt_url=yt_url)
+    
+    return render_template("home.html")
 
-        yt_download(yt_url)
-        
-    return render_template("base.html")
